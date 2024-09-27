ada79c771e935e192528ec5b48b954276f34a18f
Arthur Weber <darkw@PC-Weber>
2024-09-27 03:36:14 -0300

    add: _hero e main; remove: dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function compStyles() {
     return gulp.src('./src/styles/*scss')
@@ -7,7 +8,13 @@ function compStyles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(compStyles);
+function compImg() {
+    return gulp.src('./src/img/**/*', {encoding: false})
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+}
+
+exports.default = gulp.parallel(compStyles, compImg);
 exports.watch = function() {
     gulp.watch('./src/styles/*scss', gulp.parallel(compStyles))
 }
