33f00a0d0779a609fbded45f3b9a0019382b4dc7
Arthur Weber <darkw@PC-Weber>
2024-10-14 19:45:52 -0300

    Ajustes de compilação gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+}
 
 function compStyles() {
     return gulp.src('./src/styles/*scss')
@@ -14,7 +21,8 @@ function compImg() {
         .pipe(gulp.dest('./dist/img'));
 }
 
-exports.default = gulp.parallel(compStyles, compImg);
+exports.default = gulp.parallel(compStyles, compImg, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*scss', gulp.parallel(compStyles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
